72dc52b24b5f62413c986aa44f640fa557e328ed
erotourtes <erotourtes@gmail.com>
2025-04-22 22:47:20 +0300

    lab5: benchmark

--- lab5/run.go
+++ lab5/run.go
@@ -6,11 +6,13 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 func main() {
 	initFlag := flag.Bool("init", false, "Install dependencies")
 	cleanFlag := flag.Bool("remove", false, "Clean build files")
+	benchmark := flag.Bool("benchmark", false, "Benchmark build")
 
 	flag.Parse()
 
@@ -23,7 +25,13 @@ func main() {
 		install()
 	}
 
-	run()
+	build()
+
+	if *benchmark {
+		runBenchmark()
+	} else {
+		run()
+	}
 }
 
 const BUILD_PATH = "./build"
@@ -36,7 +44,7 @@ func install() {
 	}
 }
 
-func run() {
+func build() {
 	c := cmd("mkdir -p ./build")
 	if err := c.Run(); err != nil {
 		fmt.Println("Can't create build dir", err)
@@ -44,18 +52,55 @@ func run() {
 	}
 
 	fmt.Println("Compiling ada...")
-	c = cmd(fmt.Sprintf("gnatmake -D %s ./lab5.adb -o %s/main", BUILD_PATH, BUILD_PATH))
+	// c = cmd(fmt.Sprintf("gnatmake -D %s ./lab5.adb -o %s/main", BUILD_PATH, BUILD_PATH))
+	c = cmd(fmt.Sprintf("gnatmake -gnatp -gnatn -fPIC -D %s ./lab5.adb -o %s/main", BUILD_PATH, BUILD_PATH))
 	if err := c.Run(); err != nil {
 		fmt.Println("Compilation failed\n", err)
 		os.Exit(1)
 	}
 
+}
+
+func run() {
 	fmt.Println("\nRunning ada...")
-	c = cmd(fmt.Sprintf("%s/main", BUILD_PATH))
+	c := cmd(fmt.Sprintf("%s/main", BUILD_PATH))
 	if err := c.Run(); err != nil {
 		fmt.Println("Running failed\n", err)
 		os.Exit(1)
 	}
+}
+
+func runBenchmark() {
+	fmt.Println("\nRunning benchmark...")
+
+	runs := 4
+	durations := make([]time.Duration, runs)
+	var total time.Duration
+
+	var cores string
+	if true {
+		cores = "0"
+	} else {
+		cores = "0,1,2,3"
+	}
+
+	for i := 0; i < runs; i++ {
+		start := time.Now()
+
+		c := cmd(fmt.Sprintf("sudo taskset -c %s nice -n -20 %s/main", cores, BUILD_PATH))
+		if err := c.Run(); err != nil {
+			fmt.Println("Running failed\n", err)
+			os.Exit(1)
+		}
+		elapsed := time.Since(start)
+		durations[i] = elapsed
+		total += elapsed
+	}
+	average := total / time.Duration(runs)
+	for i, d := range durations {
+		fmt.Printf("Run %d time: %v\n", i+1, d)
+	}
+	fmt.Printf("Execution time: %v\n", average)
 }
 
 func remove() {
